8dd074579a3e6247fc3554bdb3576c52b0d5b602
Marco Rebhan <me@dblsaiko.net>
2021-10-24 00:08:47 +0200

    dev-util/jetbrains-common: Unconditionally set environment

--- dev-util/jetbrains-common/files/jbrsdk.sh
+++ dev-util/jetbrains-common/files/jbrsdk.sh
@@ -1,10 +1,10 @@
 JDK=/opt/jetbrains-jre-bin
-export CLION_JDK=${IDEA_JDK=${JDK}}
-export GOLAND_JDK=${GOLAND_JDK=${JDK}}
-export IDEA_JDK=${IDEA_JDK=${JDK}}
-export PHPSTORM_JDK=${PHPSTORM_JDK=${JDK}}
-export PYCHARM_JDK=${PYCHARM_JDK=${JDK}}
-export RIDER_JDK=${RIDER_JDK=${JDK}}
-export RUBYMINE_JDK=${RUBYMINE_JDK=${JDK}}
-export STUDIO_JDK=${STUDIO_JDK=${JDK}}
-export WEBIDE_JDK=${WEBIDE_JDK=${JDK}}
+export CLION_JDK=${JDK}
+export GOLAND_JDK=${JDK}
+export IDEA_JDK=${JDK}
+export PHPSTORM_JDK=${JDK}
+export PYCHARM_JDK=${JDK}
+export RIDER_JDK=${JDK}
+export RUBYMINE_JDK=${JDK}
+export STUDIO_JDK=${JDK}
+export WEBIDE_JDK=${JDK}
